67ac3a433c8af0ace80800ef3964b0e0c96f53fa
Jairo Gutierrez <jgutierrez@alfaconsult.com.pe>
2022-01-23 19:10:07 -0500

    se agregó entidades repositorio y dapper

---
+++ App.Pedidos.Models/Pedido.cs
@@ -0,0 +1,23 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Pedidos.Models
+{
+    public class Pedido
+    {
+        [ExplicitKey]
+        public int idPedido { get; set; }
+        public int idCliente { get; set; }
+        public int idProducto { get; set; }
+        public int idTamanio { get; set; }
+        public decimal Precio { get; set; }
+        public int Cantidad { get; set; }
+        public int Igv { get; set; }
+        public decimal PrecioTotal { get; set; }
+        public int idEstado { get; set; }
+    }
+}
---
+++ App.Pedidos.Repositories.Dapper/Repository.cs
@@ -0,0 +1,69 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Pedidos.Repositories.Dapper
+{
+    public class Repository<T>:IRepository<T> where T:class
+    {
+        protected string _connectionString;
+        public Repository(string connectionString)
+        {
+            SqlMapperExtensions.TableNameMapper = (type) => { return $"{type.Name}"; };
+            _connectionString = connectionString;
+        }
+        public Task<int> Agregar(T entidad)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                return connection.InsertAsync(entidad);
+            }
+        }
+        public Task<T> Obtener(T entidad)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                return connection.GetAsync<T>(entidad);
+            }
+        }
+        public Task<T> Obtener(int id)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                return connection.GetAsync<T>(id);
+            }
+        }
+        public Task<T> Obtener(string id)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                return connection.GetAsync<T>(id);
+            }
+        }
+        public Task<IEnumerable<T>> Listar()
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                return connection.GetAllAsync<T>();
+            }
+        }
+        public Task<bool> Eliminar(T entidad)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                return connection.DeleteAsync<T>(entidad);
+            }
+        }
+        public Task<bool> Modificar(T entidad)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                return connection.UpdateAsync<T>(entidad);
+            }
+        }
+    }
+}
