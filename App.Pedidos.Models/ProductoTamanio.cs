92284f67981f0fe52aaeb5708430f1990da22ef2
Wilmer Castro <X202114389@cibertec.edu.pe>
2022-01-24 12:08:20 -0500

    Cambios Jario:

---
+++ App.Pedidos.Models/ProductoTamanio.cs
@@ -0,0 +1,17 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Pedidos.Models
+{
+    public class ProductoTamanio
+    {
+        [ExplicitKey]
+        public int idProducto { get; set; }
+        public int idTamanio { get; set; }
+        public string Precio { get; set; }
+    }
+}
